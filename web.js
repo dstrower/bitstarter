0195926cd2606d04c9e308232c8704c58ddb64a0
Ubuntu <ubuntu@ip-10-251-35-150.us-west-2.compute.internal>
2013-07-05 22:23:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,29 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var outString = "";
+getFile();
 
-app.get('/', function(request, response) {
-  var myString = 'Hello World 3!';
-  response.send(myString);
-});
+function getFile() {
+     var myString = "";
+     fs.readFile('index.html', 
+     function (err, data) {
+       if (err) throw err;
+       outString = data.toString("utf-8", 0,data.length);
+       console.log("Outstring = " + outString);
+       startListener(outString);
+      });
+}
+function startListener(myString) {
+   app.get('/', function(request, response) {
+     response.send(myString);
+   });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+   var port = process.env.PORT || 5000;
+   app.listen(port, function() {
+     console.log("Listening on " + port);
+   });
+
+}
 
